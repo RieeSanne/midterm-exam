cf57ec3344765cb409bc564110fdee40a73c37c1
AARON T. LIZARES <2120533@cc.usls.edu>
2025-03-17 14:42:05 +0800

    final commit

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Feature;
 
 class FeatureController extends Controller
 {
-    //
+    public function index(){
+        $features = Feature::all();
+        return view('features.index', compact('features'));
+    }
 }
--- database/migrations/2025_03_17_055054_create_features_table.php
+++ database/migrations/2025_03_17_055054_create_features_table.php
@@ -9,18 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('features');
     }
